60f8b0722d5d6336dcdd2e4450421295bfaeaa6b
Michael Leisch <michael.leisch@gmx.at>
2024-03-11 11:41:30 +0100

    backend

---
+++ backend/src/main/java/org/example/repository/EntryRepository.java
@@ -0,0 +1,37 @@
+package org.example.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.example.model.Entry;
+import org.example.socket.EntrySocket;
+
+import java.util.List;
+
+@ApplicationScoped
+public class EntryRepository {
+    @Inject
+    EntityManager em;
+
+    @Inject
+    EntrySocket entrySocket;
+
+    @Transactional
+    public void addPoints(String name){
+        Entry entry = em.find(Entry.class, name);
+
+        if(entry == null){
+            Entry newEntry = new Entry(name);
+            em.persist(newEntry);
+        } else {
+            entry.increasePoints();
+        }
+
+        entrySocket.broadcast(getAll());
+    }
+
+    public List<Entry> getAll(){
+        return em.createQuery("select e from Entry e", Entry.class).getResultList();
+    }
+}
---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+let michiCount = 0;
+let yanikCount = 0;
+
+function increaseMichi(){
+    document.querySelector('.michi p').innerHTML = ++michiCount
+}
+
+function increaseYanik(){
+    document.querySelector('.yanik p').innerHTML = ++yanikCount
+}
